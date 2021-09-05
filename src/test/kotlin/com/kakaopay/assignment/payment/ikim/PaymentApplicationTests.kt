eb3f779b2bebca72acfc26df5cf50b315060a855
Ray <ray@socar.kr>
2021-09-06 08:45:58 +0900

    initial commit

---
+++ src/test/kotlin/com/kakaopay/assignment/payment/ikim/PaymentApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kakaopay.assignment.payment.ikim
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
